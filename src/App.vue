a06fae5e3cc45740c8870e664835258093bfe46c
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-23 16:40:18 +0200

    creato header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
+    <MyHeader></MyHeader>
+    <main></main>
+    <footer></footer>
   </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader.vue'
 export default {
-  name: 'App'
+  name: 'Dc-comics',
+  components: {
+    MyHeader
+  }
 }
 </script>
 
 <style>
+/* reset */
+*{
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
+}
+img{
+  max-width: 100%;
+}
+a{
+  text-decoration: none;
+}
+ul{
+  list-style-type: none;
+}
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,128 @@
+<template>
+  <header class="container">
+    <figure>
+      <a href="#"><img src="../assets/img/dc-logo.png" alt="logo"></a>
+    </figure>
+    <nav class="navbar">
+      <ul>
+        <li :class="link.current? 'active':'' " v-for="(link,i) in links " v-bind:key="'link-'+i">
+          <a @click="setActive(link)" :href="link.url">
+            {{link.text.toUpperCase()}}
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+  data: function () {
+    return {
+      links: [
+        {
+          text: 'Characters',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Comics',
+          url: '#',
+          current: true
+        },
+        {
+          text: 'Movies',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'TV',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Games',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Collectibles',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Videos',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Fans',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'News',
+          url: '#',
+          current: false
+        },
+        {
+          text: 'Shop',
+          url: '#',
+          current: false
+        }
+      ]
+
+    }
+  },
+  methods: {
+    setActive (link) {
+      this.links.forEach(cur => { cur.current = false })
+      link.current = true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img{
+  height: 100%;
+  max-width: unset;
+}
+figure{
+  height: 80%;
+}
+.container{
+    margin: 0 auto;
+    max-width: 1200px;
+    height: 100px;
+    align-items: center;
+    display: flex;
+}
+.navbar{
+  height: 100%;
+}
+ul{
+    display: flex;
+    height: 100%;
+}
+li{
+  border-bottom: 3px solid transparent;
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+}
+ul a{
+    color: black;
+    font-weight: 600;
+    font-size: 1em;
+    padding: 0 6px;
+}
+.active{
+  border-bottom: 3px solid blue;
+  a{
+    color: blue;
+  }
+}
+</style>
